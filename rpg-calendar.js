74a9014663b183011312f4b30fb0b2a7590bbd0f
Paul Stefko <ham2anv@gmail.com>
2022-11-18 08:14:22 -0500

    Build basic calendar.

--- rpg-calendar.js
+++ rpg-calendar.js
@@ -4,10 +4,25 @@ class Calendar extends HTMLElement {
     }
 
     connectedCallback() {
-        this.attachShadow({ mode: "open" }); 
+        this.attachShadow({ mode: "open" });
 
-        const grid = createElements('div','rpg-calendar');
+        const week = this.getAttribute('week') || 7;
+        const length = this.getAttribute('length');
+
+        const grid = createElement('div','rpg-calendar',{style:`--week:${week}`});
         const styles = createElement('link',{rel:"stylesheet",href:"rpg-calendar.css"});
+
+        const firstCell = createElement('div',"rpg-calendar-cell rpg-calendar-first");
+        firstCell.innerText = "1";
+        grid.append(firstCell);
+
+        if (length>1) {
+            for (let i = 0; i < length-1; i++) {
+                const cell = createElement('div','rpg-calendar-cell');
+                cell.innerText = i+2;
+                grid.append(cell);
+            }
+        }
 
         
         this.shadowRoot.append(grid, styles);
@@ -21,7 +36,7 @@ customElements.define("rpg-calendar",Calendar);
 function createElement(element, styles=null, props=null) {
     if (arguments.length == 2 && typeof styles == "object") props = styles;
     const newElement = document.createElement(element);
-    if (styles && props != styles) newElement.classList.add(styles.split(' '));
+    if (styles && props != styles) newElement.classList.add(...styles.split(' '));
     if (props) {
         for (let [key,value] of Object.entries(props)) {
             newElement.setAttribute(key,value);
